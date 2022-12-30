97a339f32b39a2781760cbf1b930163a3fa4e4c4
Hello-World-1972 <kmrsanthoshofficial@gmail.com>
2022-12-30 09:23:59 +0530

    Initial commit

---
+++ lib/src/constants/text_strings.dart
@@ -0,0 +1,87 @@
+/* -- App Text Strings -- */
+
+// -- GLOBAL Texts
+const String tNext = "Next";
+const String tLogin = "Login";
+const String tEmail = "E-Mail";
+const String tSignup = "Signup";
+const String tPhoneNo = "Phone No";
+const String tPassword = "Password";
+const String tFullName = "Full Name";
+const String tForgetPassword = "Forget Password";
+const String tSignInWithGoogle = "Sign-In with Google";
+late String tusername;
+
+
+// -- Splash Screen Text
+const String tAppName = "DysThoughts";
+const String tAppTagLine = "Screen for SLD \nFree For Everyone";
+
+
+// -- On Boarding Text
+const String tOnBoardingTitle1 = "Build Awesome Apps";
+const String tOnBoardingTitle2 = "Learn from YouTube";
+const String tOnBoardingTitle3 = "Get Code & Resources";
+const String tOnBoardingSubTitle1 = "Let's start your journey with us on this amazing and easy platform.";
+const String tOnBoardingSubTitle2 = "Get Video Tutorials of each topic to learn things easily.";
+const String tOnBoardingSubTitle3 = "Save time by just copy pasting complete apps you learned from videos.";
+const String tOnBoardingCounter1 = "1/3";
+const String tOnBoardingCounter2 = "2/3";
+const String tOnBoardingCounter3 = "3/3";
+
+
+// -- Welcome Screen Text
+const String tWelcomeTitle = "Welcome to DysThoughts";
+const String tWelcomeSubTitle = "The only app to screen for SLD at no cost!";
+
+
+// -- Login Screen Text
+const String tLoginTitle = "Welcome Back,";
+const String tLoginSubTitle = "Make it work, make it right, make it fast.";
+const String tRememberMe = "Remember Me?";
+const String tDontHaveAnAccount = "Don't have an Account? ";
+
+
+// -- Sign Up Screen Text
+const String tSignUpTitle = "Get On Board!";
+const String tSignUpSubTitle = "Create your profile to start your Journey.";
+const String tAlreadyHaveAnAccount = "Already have an Account? ";
+
+
+// -- Forget Password Text
+const String tForgetPasswordTitle = "Make Selection!";
+const String tForgetPasswordSubTitle = "Select one of the options given below to reset your password.";
+const String tResetViaEMail = "Reset via Mail Verification";
+const String tResetViaPhone = "Reset via Phone Verification";
+
+// -- Forget Password Via Phone - Text
+const String tForgetPhoneSubTitle = "Enter your registered Phone No to receive OTP";
+
+// -- Forget Password Via E-Mail - Text
+const String tForgetMailSubTitle = "Enter your registered E-Mail to receive OTP";
+
+// -- OTP Screen - Text
+const String tOtpTitle = "CO\nDE";
+const String tOtpSubTitle = "Verification";
+const String tOtpMessage = "Enter the verification code sent at ";
+
+// -- Dashboard Screen - Text
+const String tDashboardTitle = "Hey, Coding with T";
+const String tDashboardHeading = "Explore Courses";
+const String tDashboardSearch = "Search...";
+const String tDashboardBannerTitle1 = "Android for Beginners";
+const String tDashboardBannerTitle2 = "JAVA";
+const String tDashboardBannerSubTitle = "10 lessons";
+const String tDashboardButton = "View All";
+const String tDashboardTopCourses = "Top Courses";
+
+// -- Test 1 - Listening Assessment
+const String tListeningAssessment = "Listening Assessment";
+const String tAnswer = "Answer";
+const listeningAssessmentQN1 = "https://firebasestorage.googleapis.com/v0/b/software-engineering-pro-21553.appspot.com/o/listening_assessment%2Flistening_assessment_1.mp3?alt=media&token=5bd62d8a-703a-4ee3-9bf2-6bf609488366";
+const listeningAssessmentQN2 = "https://firebasestorage.googleapis.com/v0/b/software-engineering-pro-21553.appspot.com/o/listening_assessment%2Flistening_assessment_2.mp3?alt=media&token=315ece95-ba1d-44af-9bf9-7b550bda3d80";
+
+const ishiharaAssessmentPic1 = "https://firebasestorage.googleapis.com/v0/b/software-engineering-pro-21553.appspot.com/o/ishihara_assessment%2Fishihara_assessment_1.png?alt=media&token=79855d1f-8929-4fc2-857a-2b7414b1aed0";
+const ishiharaAssessmentPic2 = "https://firebasestorage.googleapis.com/v0/b/software-engineering-pro-21553.appspot.com/o/ishihara_assessment%2Fishihara_assessment_2.png?alt=media&token=999ee1ee-9e7b-4d61-868a-4a955fe7baac";
+const mathAssessmentPic1 = "https://firebasestorage.googleapis.com/v0/b/software-engineering-pro-21553.appspot.com/o/math_assessment%2Fmath_assessment_1.png?alt=media&token=6f2fb997-256b-4c57-a1d8-cd8045714fce";
+const mathAssessmentPic2 = "https://firebasestorage.googleapis.com/v0/b/software-engineering-pro-21553.appspot.com/o/math_assessment%2Fmath_assessment_2.png?alt=media&token=27ba3040-e4e3-4ec4-8603-68602ece0a6f";
